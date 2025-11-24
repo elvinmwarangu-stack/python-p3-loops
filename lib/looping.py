ab4646c0c05a66379fff56e2806ad80cd589395c
Elvin <elvin.mwarangu@student.moringaschool.com>
2025-11-24 09:05:26 +0300

    solution done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    # Print numbers 10 down to 1
+    for i in range(10, 0, -1):
+        print(i)
+    # Final message
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    # Return a list of squares
+    return [i * i for i in int_list]
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    # Loop from 1 to 100
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
